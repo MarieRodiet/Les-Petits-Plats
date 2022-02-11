dbcd5a8b897f9de2f50af5297bd0f20df41a878a
Marie Rodiet <marie.rodiet@gmail.com>
2022-02-11 20:37:47 +0100

    retrieving all data from json

--- scripts/page/index.js
+++ scripts/page/index.js
@@ -2,20 +2,51 @@ import Api from "./../api/api.js";
 
 class App {
     constructor() {
-        this.data = null;
-        
+        this.allData = null;
+        this.allRecipeNames = null;
+        this.allApliances = null;
+        this.allIngredients = null;
+
+
     }
 
     async init() {
-        const data = await this.getData();
-        this.data = data;
-        console.log(this.data);
+        await this.getData();
     }
 
     async getData() {
         const api = new Api("data/recipes.json");
-        const data = await api.getJsonData();
-        return data;
+        this.allData = await api.getJsonData();
+        console.log("allData: ");
+        console.log(this.allData);
+        this.allRecipeNames = this.retrieveData("name", false);
+        console.log("allRecipeNames: ");
+        console.log(this.allRecipeNames);
+        this.allApliances = this.retrieveData("appliance", true);
+        console.log("allAppliances: ");
+        console.log(this.allApliances);
+        this.allIngredients = this.retrieveIngredients();
+        console.log("allIngredients: ");
+        console.log(this.allIngredients);
+
+
+
+    }
+
+    retrieveData(type, removeDouble) {
+        let result = this.allData.map(element => element[type]);
+        if (removeDouble) {
+            let unique = result.filter((c, index) => {
+                return result.indexOf(c) === index;
+            })
+            result = unique;
+        }
+        return result;
+    }
+
+    retrieveIngredients() {
+        let result = this.allData.map((element) => element["ingredients"][0]["ingredient"]);
+        return result;
     }
 }
 
